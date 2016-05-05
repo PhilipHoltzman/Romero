61d7df6f278a8579653e419a7f03b1f8ad4ead76
philipholtzman <phil.holtzman@gmail.com>
2016-05-05 16:17:31 -0500

    basic xy movement

--- romero.py
+++ romero.py
@@ -2,13 +2,60 @@ import pygame
 
 pygame.init()
 
+# pallette 
+turquoise1 = (0,245,255)
+turquoise2 = (0,229,238)
+turquoise3 = (0,197,205)
+turquoise4 = (0,134,139)
+black = (0,0,0)
+white = (255,255,255)
+
 gameDisplay = pygame.display.set_mode((800,600))
-
-pygame.display.update()
-
+pygame.display.set_caption('Snakemero')
 
 
 
+gameExit = False
+
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+lead_y_change = 0
+
+clock = pygame.time.Clock()
+
+while not gameExit:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				lead_x_change = -6
+			if event.key == pygame.K_RIGHT:
+				lead_x_change = 6
+			if event.key == pygame.K_UP:
+				lead_y_change = -6
+			if event.key == pygame.K_DOWN:
+				lead_y_change = 6
+
+	# Stop code for non endless moving mode
+	#	if event.type == pygame.KEYUP:
+	#		if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+	#			lead_x_change = 0
+	#	if event.type == pygame.KEYUP:
+	#		if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+	#			lead_y_change = 0
+
+	lead_x += lead_x_change
+	lead_y += lead_y_change
+
+
+	gameDisplay.fill(black)
+	pygame.draw.rect(gameDisplay,turquoise4,[lead_x,lead_y,10,10])
+	pygame.display.update()
+
+	clock.tick(15)
 
 
 pygame.quit()
